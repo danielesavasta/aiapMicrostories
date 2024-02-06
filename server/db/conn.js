70f2f5d4d5b222ff55f4f237747acb90a54b9693
Daniele Savasta <daniele.savasta@gmail.com>
2024-02-06 18:18:32 +0100

    .

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,31 +1,12 @@
 //const MongoClient = require("mongodb").MongoClient;
 //const connectionString = process.env.MONGOD_CONNECT_URI; //?directConnection=true
-/*
+
 const client = new MongoClient(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+  }/*);
   
-  let dbConnection;
-
-  module.exports = {
-    connectToServer: function (callback) {
-      client.connect(function (err, db) {
-        if (err || !db) {
-          return callback(err);
-        }
-  
-        dbConnection = db.db("aiap");
-        console.log("Successfully connected to MongoDB.");
-  
-        return callback();
-      });
-    },
-  
-    getDb: function () {
-      return dbConnection;
-    },
-  };*/
+  */
 
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -53,3 +34,24 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+let dbConnection;
+
+  module.exports = {
+    connectToServer: function (callback) {
+      client.connect(function (err, db) {
+        if (err || !db) {
+          return callback(err);
+        }
+  
+        dbConnection = db.db("aiap");
+        console.log("Successfully connected to MongoDB.");
+  
+        return callback();
+      });
+    },
+  
+    getDb: function () {
+      return dbConnection;
+    },
+  };
