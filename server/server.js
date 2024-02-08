bf882b6c0aef169db4ef490eccea9fbcf9dd864a
Daniele Savasta <daniele.savasta@gmail.com>
2024-02-08 17:04:52 +0100

    .

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 // get MongoDB driver connection
-const dbo = require('./db/conn');
+//const dbo = require('./db/conn');
 
 const PORT = process.env.PORT || 10000;
 const app = express();
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
-app.use(require('./routes/record'));
+// app.use(require('./routes/record'));
 app.use(express.json());
 
 // Global error handling
@@ -26,6 +26,7 @@ app.use(function (err, _req, res) {
 });*/
 
 // perform a database connection when the server starts
+/*
 dbo.connectToServer(function (err) {
   if (err) {
     console.error(err);
@@ -38,3 +39,49 @@ dbo.connectToServer(function (err) {
    // console.log(collection1);
   });
 });
+*/
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = process.env.MONGOD_CONNECT_URI;
+
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+     */
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
